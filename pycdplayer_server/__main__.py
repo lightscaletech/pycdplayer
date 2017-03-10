d49d8dd2e28e6ef544e4e5f7b47af567f76b3490
Sam Light <samlight1994@gmail.com>
2017-03-10 10:12:31 +0000

    Run player from server

--- pycdplayer_server/__main__.py
+++ pycdplayer_server/__main__.py
@@ -1,7 +1,4 @@
-from libcdplayer import libcdplayer
 from pycdplayer_server import server
 
-player = libcdplayer.Player()
-player.play()
-server = server.Server(player)
+server = server.Server()
 server.run()
--- pycdplayer_server/server.py
+++ pycdplayer_server/server.py
@@ -1,16 +1,55 @@
+from libcdplayer import libcdplayer
+from http import HTTPStatus
 from http.server import HTTPServer, SimpleHTTPRequestHandler
+
+import json
+
+player = libcdplayer.Player()
+player.play()
+
 
 class Server():
     class APIHandler(SimpleHTTPRequestHandler):
+
+        CODE_SUCCESS   = HTTPStatus.OK
+        CODE_NOT_FOUND = HTTPStatus.NOT_FOUND
+        CODE_ERROR     = HTTPStatus.INTERNAL_SERVER_ERROR
+
+        def resp(self, c, d = ''): return (c, bytes(json.dumps(d), 'utf-8'))
+        def success(self, d):       return self.resp(self.CODE_SUCCESS, d)
+        def error(self, d):         return self.resp(self.CODE_ERROR, d)
+        def not_found(self):        return self.resp(self.CODE_NOT_FOUND)
+
+        def track(self, segs):
+            if segs[0] == 'set':
+                num = int(segs[1])
+                player.set_track(num)
+                return self.success({'status': 'success'})
+            return self.not_found()
+
+
+        def route(self):
+            path = self.path
+            segs = []
+            for s in path.split('/'):
+                if len(s) > 0: segs.append(s)
+
+            if segs[0] == 'track': self.track(segs[1:])
+            print (path)
+            print (segs)
+
+            return self.not_found()
+
         def do_GET(self):
-            self.send_response(200)
+            code, data = self.route()
+            self.send_response(code)
             self.send_header('content-type', 'application/json')
-            self.wfile.write(b"My name is Sam!")
+            self.end_headers()
+            self.wfile.write(data)
 
-
-    def __init__(self, player):
-        self.player = player
+    def __init__(self): pass
 
     def run(self):
+        player.play()
         self.httpserv = HTTPServer(("", 9111), self.APIHandler)
         self.httpserv.serve_forever()
