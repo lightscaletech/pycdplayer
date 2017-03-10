584d267e90b8735d01960cd9234ecb1c552fea21
Sam Light <samlight1994@gmail.com>
2017-03-10 07:59:04 +0000

    Created basic server.

--- pycdplayer_server/server.py
+++ pycdplayer_server/server.py
@@ -1,2 +1,16 @@
-from SimpleHTTPServer  import SimpleHTTPRequestHandler
-import SocketServer
+from http.server import HTTPServer, SimpleHTTPRequestHandler
+
+class Server():
+    class APIHandler(SimpleHTTPRequestHandler):
+        def do_GET(self):
+            self.send_response(200)
+            self.send_header('content-type', 'application/json')
+            self.wfile.write(b"My name is Sam!")
+
+
+    def __init__(self, player):
+        self.player = player
+
+    def run(self):
+        self.httpserv = HTTPServer(("", 9111), self.APIHandler)
+        self.httpserv.serve_forever()
